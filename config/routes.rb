5ace272749cb387a4035a95353c55d059b3e25fb
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2023-10-05 19:52:26 -0500

    Create controller and view for cocktail index and cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "cocktails#index"
+
+  get "/cocktails", to: "cocktails#index"
+  get "/cocktails/:id", to: "cocktails#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
