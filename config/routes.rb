c6194ba8f7138eb8aea1db27904ecc0534003449
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2023-10-09 11:35:52 -0500

    Changed routes to use rail's resources method

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   root "cocktails#index"
 
-  get "/cocktails", to: "cocktails#index"
-  get "/cocktails/:id", to: "cocktails#show"
+
+  resources :cocktails
+
+  #get "/cocktails", to: "cocktails#index"
+  #get "/cocktails/:id", to: "cocktails#show"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
