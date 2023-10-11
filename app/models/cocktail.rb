88214cca9487ef171c8c948f0c09c37a065dbe2f
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2023-10-11 11:59:03 -0500

    Added way of creating new entries for cocktail recipes using rail's form builder. Changes made to controller, model, and views to support this feature.

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,6 @@
 class Cocktail < ApplicationRecord
+  validates :name, presence: true
+  validates :image_url, presence: true
+  validates :ingredient, presence: true
+  validates :instruction, presence: true
 end
