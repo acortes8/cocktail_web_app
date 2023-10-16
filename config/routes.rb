2c52c60eb2f20958cfb4e83b6fd9a7ab5fa79912
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2023-10-16 17:31:37 -0500

    Remove old code that was commented out

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
   root "cocktails#index"
 
-
   resources :cocktails
-
-  #get "/cocktails", to: "cocktails#index"
-  #get "/cocktails/:id", to: "cocktails#show"
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
 end
