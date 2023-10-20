145ce4e9161d2216a18500bbea0564beed17bcb0
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2023-10-20 01:03:10 -0500

    Fix bug that created duplicate ingredients instead of using ones that already exist when creating new cocktails

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -8,4 +8,12 @@ class Cocktail < ApplicationRecord
   validates :spirit_type, presence: true
 
   accepts_nested_attributes_for :cocktail_ingredients, allow_destroy: true
+
+  before_save :find_or_create_ingredients
+
+  def find_or_create_ingredients
+    self.cocktail_ingredients.each do |cocktail_ingredient|
+      cocktail_ingredient.ingredient = Ingredient.find_or_create_by(name:cocktail_ingredient.ingredient.name)
+    end
+  end
 end
